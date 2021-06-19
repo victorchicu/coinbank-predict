2ecbef93049b4dc844bdfdfe0fea5ae2656a5046
Victor Chicu <victor@aroundwire.com>
2021-06-20 01:23:44 +0300

    Prepare datasets (WIP)

--- api/Cryptocompare.py
+++ api/Cryptocompare.py
@@ -1,10 +1,10 @@
 import requests
 
 API_KEY = "f01df1555a57232243f497f5fa31ab3ae984e45cc74c992e564ca79def08de4b"
-BASE_URL = "https://min-api.cryptocompare.com/data"
+BASE_URL = "https://min-api.cryptocompare.com"
 
 
-def get_symbol_price(symbol, fiat):
-    url = BASE_URL + "/price?fsym=" + symbol + "&tsyms=" + fiat + "&api_key" + API_KEY;
+def get_historical_data_of_the_day(symbol, end_of_day):
+    url = BASE_URL + "/data/v2/histominute?fsym=" + symbol + "&tsym=USD" + "&limit=" + str(1440) + "&toTs=" + str(end_of_day) + "&api_key=" + API_KEY
     response = requests.get(url)
     return response.json()
--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from api import Cryptocompare
+import os
+import json
+import pandas as pd
+import datetime as dt
 
 app = Flask(__name__)
 
 
-@app.route('/price/<symbol>/to/<fiat>')
-def get_crypto_price(symbol, fiat):
-    return Cryptocompare.get_symbol_price(symbol, fiat)
-
+@app.route('/test')
+def prepare_datasets():
+    for i in range(7, 0, -1):
+        end_of_day = dt.datetime.combine(dt.datetime.today() - dt.timedelta(days=i),  dt.time(23, 59, 59))
+        ts = int(end_of_day.timestamp())
+        response = Cryptocompare.get_historical_data_of_the_day(symbol="BTC", end_of_day=ts)
+        sub_data = response["Data"]["Data"]
+        json_string = json.dumps(sub_data);
+        df_series = pd.read_json(json_string)
+        df_series.drop(["conversionType", "conversionSymbol"], axis=1, inplace=True)
+        file_name = str(dt.datetime.fromtimestamp(ts).date()) + ".csv"
+        output_file = os.path.join("/home/victor_chicu/IdeaProjects/crypto/crypto-predict/static", file_name)
+        df_series.to_csv(output_file, index=False)
+    return "OK"
 
 if __name__ == '__main__':
     app.run()
